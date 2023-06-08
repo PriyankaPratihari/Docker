08ed4ab4f6f1d0ddb8df08fa7f383ec06013e445
PriyankaPratihari <132648511+PriyankaPratihari@users.noreply.github.com>
2023-06-08 12:46:35 +0530

    app.py
    updated

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uvicorn
+from script.config import service_data
 # from script.config import service_data
 
 if __name__ == '__main__':
-    uvicorn.run("main:app")
+    uvicorn.run("main:app",reload=True,host=service_data.host,port=service_data.host)
